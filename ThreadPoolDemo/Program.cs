63a0f16c2d96e9278b2eccab7734e6e310cd3bd4
aronszucs <szucsaron@hotmail.com>
2019-07-25 10:52:15 +0200

    task completed

--- ThreadPoolDemo/Program.cs
+++ ThreadPoolDemo/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ThreadPoolDemo
 {
     class Program
     {
+        static void ShowMyText(object state)
+        {
+            string text = state.ToString();
+            text += " Thread: " + Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine(text);
+        }
         static void Main(string[] args)
         {
+            WaitCallback callback = new WaitCallback(ShowMyText);
+            ThreadPool.QueueUserWorkItem(callback, "Hello");
+            ThreadPool.QueueUserWorkItem(callback, "Hi");
+            ThreadPool.QueueUserWorkItem(callback, "Heya");
+            ThreadPool.QueueUserWorkItem(callback, "Goodbye");
+            Console.ReadKey();
         }
     }
 }
